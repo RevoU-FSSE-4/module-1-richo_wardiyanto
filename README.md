38c40e06ce20a32e31a7c24a442d352f50b8652a
Richo Wardiyanto <richowardiyanto1@gmail.com>
2024-02-16 15:23:31 +0700

    add html in module 1 assigment

---
+++ README.md
@@ -0,0 +1,6 @@
+# Solana Check Address 
+
+## Overview
+
+Solana is a high-performance blockchain platform designed to support decentralized applications (dApps) and cryptocurrencies at scale. It aims to provide fast, secure, and scalable infrastructure for developers to build decentralized applications without facing the bottlenecks of traditional blockchain architectures.
+
---
+++ public/scripts.js
@@ -0,0 +1,29 @@
+async function checkAddress() {
+    var address = document.getElementById("solanaAddress").value.trim();
+    var resultMessage = document.getElementById("resultMessage");
+
+    // Initialize a connection to the Solana mainnet
+    const connection = new solanaWeb3.Connection(
+        solanaWeb3.clusterApiUrl('devnet')
+    );
+
+    try {
+        // Fetch account information for the provided address
+        const accountInfo = await connection.getAccountInfo(
+            new solanaWeb3.PublicKey(address)
+        );
+
+        // If accountInfo is null, the address does not exist on the mainnet
+        if (accountInfo === null) {
+            resultMessage.innerText = "Solana address does not exist on the mainnet.";
+        } else {
+            const balance = accountInfo.lamports / solanaWeb3.LAMPORTS_PER_SOL;
+            balanceMessage.innerText = `Balance of ${balance} SOL`;
+            resultMessage.innerText = "Solana address exists on the mainnet.";
+
+        }
+    } catch (error) {
+        console.error("Error occurred while checking Solana address:", error);
+        resultMessage.innerText = "Error occurred while checking Solana address. See console for details.";
+    }
+}
